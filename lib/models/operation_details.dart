31a76a54fcfdcae6db5ea4956b3c0bab45dc5a2c
Ivan Dario Avila Martinez <idamkiller@hotmail.com>
2025-07-16 11:42:10 -0500

    feat(operation-details): create OperationDetails class for operation metadata

---
+++ lib/models/operation_details.dart
@@ -0,0 +1,13 @@
+import 'package:example_mutation_test/operations.dart';
+
+class OperationDetails {
+  final Operations operation;
+  final String description;
+  final String function;
+
+  OperationDetails({
+    required this.operation,
+    required this.description,
+    required this.function,
+  });
+}
